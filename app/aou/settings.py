5d486915bff7b6a5a819985d32ba1a40924ccacf
yoonlight <yoonlight12@gmail.com>
2022-06-12 22:43:09 +0900

    [ #8 ] feat: connect mariadb to django

--- app/aou/settings.py
+++ app/aou/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'aou.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'USER': os.environ.get('DB_USER'),
+        'NAME': os.environ.get('DB_NAME'),
+        'PASSWORD': os.environ.get('DB_PASS'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
 
